fa9492f2bf730099f90a7417bf63c8361661eb1f
wilsonrc <wilsonrc16@gmail.com>
2019-03-24 17:26:45 -0400

    add: users activity add: users activity layout.

---
+++ app/src/main/java/com/wilsonrc/githubusers/ui/users/UsersActivity.kt
@@ -0,0 +1,13 @@
+package com.wilsonrc.githubusers.ui.users
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.wilsonrc.githubusers.R
+
+class UsersActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_users)
+    }
+}

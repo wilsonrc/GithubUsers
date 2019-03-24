557d2145e0e10f629d1e9ef157673c111156736e
wilsonrc <wilsonrc16@gmail.com>
2019-03-23 21:14:51 -0400

    add: user remote model.

---
+++ app/src/main/java/com/wilsonrc/githubusers/data/models/User.kt
@@ -0,0 +1,23 @@
+package com.wilsonrc.githubusers.data.models
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class User {
+
+    @SerializedName("login")
+    @Expose
+    var name: String? = null
+
+    @SerializedName("id")
+    @Expose
+    var id: Int? = null
+
+    @SerializedName("avatar_url")
+    @Expose
+    var avatarUrl: String? = null
+
+    @SerializedName("url")
+    @Expose
+    var url: String? = null
+}
